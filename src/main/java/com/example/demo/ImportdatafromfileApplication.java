4515b3fb72ef29f5d3b1be94e30ea40c10989aac
2770220486@qq.com <15892000czy>
2018-03-01 10:54:15 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/ImportdatafromfileApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportdatafromfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportdatafromfileApplication.class, args);
+	}
+}
